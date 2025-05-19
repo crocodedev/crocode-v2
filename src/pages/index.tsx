4d966c000cf0005288b4da667a37b92d2e9f28a3
Valery Natykan <valeriy.natykan@cookies.games>
2025-05-19 16:07:44 +0300

    fix: delete all components on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,7 @@
-import {
-  AboutUsSection,
-  BlogSection,
-  CallerSection,
-  IndustriesSection,
-  OurProjectsSection,
-  ServicesSection,
-  TechnologiesSection,
-} from '@/components/sections';
-import { Banner } from '@/components/ui';
-
 export default function Home() {
   return (
     <div>
       <h1>crocode v2</h1>
-      <IndustriesSection />
-      <ServicesSection />
-      <Banner title='ABOUT CROCODE' />
-      <TechnologiesSection />
-      <CallerSection />
-      <OurProjectsSection />
-      <AboutUsSection />
-      <BlogSection />
     </div>
   );
 }
