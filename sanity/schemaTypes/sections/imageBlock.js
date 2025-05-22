183dea996b73b93979ab272127e2dc52d062f90f
dmitryn <dmitryn.crocode@gmail.com>
2025-05-22 14:42:25 +0300

    add sanity schemas from crocode-v1

---
+++ sanity/schemaTypes/sections/imageBlock.js
@@ -0,0 +1,24 @@
+export default {
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+
+  title: 'Image block',
+  name: 'imageBlock',
+  type: 'document',
+  i18n: true,
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'imageWithAlt',
+    },
+  ],
+}
---
+++ sanity/schemaTypes/sections/technologies.js
@@ -0,0 +1,72 @@
+export default {
+  preview: {
+    select: {
+      title: 'sectionTitle',
+    },
+  },
+
+  title: 'Technologies',
+  name: 'technologies',
+  type: 'document',
+  i18n: true,
+  fields: [
+    {
+      title: 'Section Title',
+      name: 'sectionTitle',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Position',
+      name: 'position',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Component',
+      name: 'component',
+      type: 'string',
+      initialValue: 'Technologies',
+      options: {
+        list: [
+          {title: 'Technologies', value: 'Technologies'},
+          {title: 'Technologies Grid', value: 'TechnologiesGrid'},
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Subtitle',
+      name: 'subtitle',
+      type: 'text',
+    },
+    {
+      title: 'Categories',
+      name: 'categories',
+      type: 'array',
+      of: [
+        {
+          type: 'technologyCategory',
+        },
+      ],
+    },
+    {
+      title: 'Breadcrumbs',
+      name: 'breadcrumbs',
+      type: 'array',
+
+      of: [
+        {
+          name: 'link',
+          type: 'link',
+        },
+      ],
+    },
+  ],
+}
