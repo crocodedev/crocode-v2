c539324983cbfc00cccfc0553145f6e3024e80cb
Valery Natykan <valeriy.natykan@cookies.games>
2025-05-29 14:58:12 +0300

    fix: use array & .map for create models

--- src/components/sections/models-section-template/index.tsx
+++ src/components/sections/models-section-template/index.tsx
@@ -5,47 +5,62 @@ import { MODELS } from '@/utils/const';
 import styles from './styles.module.scss';
 import PrimitiveFactory from '@/lib/three/PrimitiveFactory';
 
+const MODELS_LIST = [
+  {
+    prompt: MODELS.ROGUE.url,
+    className: styles.rogue,
+    lightIntensity: 2,
+    rotation: [Math.PI / 10, 0, 0] as [number, number, number],
+    isHaveTexture: true,
+  },
+  {
+    prompt: MODELS.HOLLOW_PLANET.url,
+    className: styles.hollow_planet_1,
+    isHaveTexture: true,
+    lightDirectionPosition: [40, 20, 20] as [number, number, number],
+    rotation: [Math.PI / 3.2, 0, -Math.PI * 0.25] as [number, number, number],
+    lightIntensity: 5,
+  },
+  {
+    prompt: MODELS.CROSS.url,
+    className: styles.cross,
+    isHaveTexture: true,
+    lightIntensity: 2.5,
+    rotateY: Math.PI / 6,
+    rotation: [-Math.PI * 0.55, Math.PI * 0.65, Math.PI * 0.45] as [
+      number,
+      number,
+      number,
+    ],
+  },
+  {
+    prompt: MODELS.HOLLOW_PLANET.url,
+    className: styles.hollow_planet_2,
+    isHaveTexture: true,
+    lightIntensity: 5,
+    cameraPosition: [1.5, 1.8, 1.8] as [number, number, number],
+    lightDirectionPosition: [10, 5, 5] as [number, number, number],
+    rotation: [Math.PI * 0.3, Math.PI * 0.18, Math.PI * 0.3] as [
+      number,
+      number,
+      number,
+    ],
+  },
+  {
+    prompt: MODELS.HOLLOW_PLANET.url,
+    className: styles.hollow_planet_3,
+    isHaveTexture: true,
+    lightIntensity: 5,
+    rotation: [Math.PI / 3, 0, 0] as [number, number, number],
+  },
+];
+
 const ModelsSectionTemplate = ({ children }: PropsWithChildren) => {
   return (
     <Fragment>
-      <PrimitiveFactory
-        scale={1}
-        prompt={MODELS.PLANET.url}
-        className={styles.planet}
-        rotation={[-Math.PI / 1.3, 0, 0]}
-        animation={{
-          rotationSpeed: 0.1,
-          bounceHeight: 0.2,
-          bounceSpeed: 0.25,
-          rotationAxis: 'bounce',
-        }}
-        lightIntensity={10}
-      />
-      <PrimitiveFactory
-        scale={1}
-        prompt={MODELS.MARSHMALLOW.url}
-        className={styles.marshmallow}
-        rotation={[-Math.PI / 1.3, 0, 0]}
-        animation={{
-          rotationSpeed: 0.1,
-          bounceHeight: 0.2,
-          bounceSpeed: 0.25,
-          rotationAxis: 'bounce',
-        }}
-      />
-      <PrimitiveFactory
-        scale={0.8}
-        prompt={MODELS.FLUTED_PIPE.url}
-        className={styles.fluted_pipe}
-        rotation={[Math.PI / 1.5, -7.5, -8]}
-        animation={{
-          rotationSpeed: 0.1,
-          bounceHeight: 0.2,
-          bounceSpeed: 0.25,
-          rotationAxis: 'bounce',
-        }}
-        lightIntensity={10}
-      />
+      {MODELS_LIST.map((model, index) => (
+        <PrimitiveFactory key={index} {...model} />
+      ))}
       {children}
     </Fragment>
   );
