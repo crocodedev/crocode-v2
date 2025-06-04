82fcc8f5cb08fc02451878660ad0be4845640f9f
Ruslan Gumnov <ruslang.crocode@gmail.com>
2025-06-04 15:42:14 +0300

    intermediate

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -31,7 +31,6 @@ type TProps = {
 };
 
 const Footer = (props: TProps) => {
-  console.log(props);
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -1,10 +1,30 @@
+import Image from 'next/image';
+
+import { TImage, TLink } from '@/types/types';
+
+import SectionLayout from '../section-layout';
+
 import styles from './styles.module.scss';
 
-const Hero = () => {
+type TProps = {
+  title: string;
+  imageWithAltText: TImage;
+  breadcrumbs: TLink[];
+};
+
+const Hero = (props: TProps) => {
+  console.log(props);
   return (
-    <section className={styles.hero}>
-      <h1 className={styles.hero__title}>crocode</h1>
-    </section>
+    <SectionLayout className={styles.hero}>
+      <div className={styles.hero__breadcrumbs}></div>
+      <Image
+        className={styles.hero__background}
+        src={props.imageWithAltText.image.asset.url}
+        fill
+        alt={props.imageWithAltText.altText}
+      />
+      <h1 className={styles.hero__title}>{props.title}</h1>
+    </SectionLayout>
   );
 };
 
